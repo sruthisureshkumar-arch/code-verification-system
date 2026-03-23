a8d3452138fb430ac7303f463d5897dadd350885
sruthisureshkumar-arch <sruthisureshkumar@gmail.com>
2026-03-23 21:34:46 +0530

    Fix: Remove non-existent Angular provider that caused build failure

--- angular-frontend/src/app/app.config.ts
+++ angular-frontend/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 
@@ -6,7 +6,6 @@ import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient()
   ]
